f8fa2e801f45d7b9801940fd52afafe2b2ce10a9
DrumPire <expresproject@gmail.com>
2022-02-14 20:20:33 +0200

    added mobile menu and modal window for watch trailer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
 const swiper = new Swiper('.main-slider', {
-  slidesPerView: 2,
   spaceBetween: 28,
   loop: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    }
+  },
 
   navigation: {
     nextEl: '.main-slider__arrow',
   },
+});
+
+const byrgerBtnOpen = document.querySelector('.menu-burger');
+const byrgerBtnClose = document.querySelector('.header__menu-close');
+const mobileMenu = document.querySelector('.header');
+const modalWindow = document.querySelector('.modal');
+const playTrailerButton = document.querySelector('.main__button-play');
+
+byrgerBtnOpen.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
+});
+
+byrgerBtnClose.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
+});
+
+playTrailerButton.addEventListener('click', () => {
+  modalWindow.classList.add('active');
+});
+
+modalWindow.addEventListener('click', e => {
+  const isModal = e.target.closest('.modal__inner');
+
+  if (!isModal) {
+    modalWindow.classList.remove('active');
+  }
 });
